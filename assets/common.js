c04d7aabe9d19fb9cde4229a3dfab427e5f99855
Anuj Shrivastava <anujshrivastava10e@gmail.com>
2024-09-15 18:34:38 +0530

    Add fls

--- assets/common.js
+++ assets/common.js
@@ -1,3 +1,6 @@
 // const API_KEY = "3TMBVUTaNCG6G71BJjuw34rQqwAsE58cAMBteWw1zxD7";
 const API_KEY = "p6re8UxEBCW4mSL7LNycA22dvsc9yibFmD7omqsythI";
 
+function convert_to_json(data) {
+    return data.json()
+}
